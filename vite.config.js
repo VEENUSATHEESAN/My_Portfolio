574468e08ef521f3dd000d689bdeaed068e506ce
veenusatheesan <veenusatheesan@gmail.com>
2025-02-28 16:05:59 +0530

    update 2

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
-export default {
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],  // Ensures React support in Vite
   build: {
     sourcemap: true,
   },
-};
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-dom/client']
+  }
+});
